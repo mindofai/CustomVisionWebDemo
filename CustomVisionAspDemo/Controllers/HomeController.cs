7bfcea1cbe155d933e03dbed93016d3d8ef48d7a
Bryan Anthony Garcia <bryan@xam-consulting.com>
2019-04-01 12:34:49 +0800

    Add project files.

---
+++ CustomVisionAspDemo/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CustomVisionAspDemo.Models;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace CustomVisionAspDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new IndexModel());
+        }
+
+        public async Task<IActionResult> Predict(IFormFile file)
+        {
+            var image = file.OpenReadStream();
+
+            if (image != null)
+            {
+                CustomVisionPredictionClient endpoint = new CustomVisionPredictionClient()
+                {
+                    ApiKey = "4a6f15ad57c941b1b72e215f6f69c410",
+                    Endpoint = "https://southeastasia.api.cognitive.microsoft.com/"
+                };
+
+                var result = await endpoint.ClassifyImageAsync(new Guid("6e8a2de4-a37d-4649-b9f6-d30f9a53052d"),
+                    "Iteration1", image);
+
+                return View("Index", new IndexModel() { Image = convertFileToString(file), Prediction = result.Predictions[0].TagName });
+            }
+
+            return null;
+        }
+
+        private string convertFileToString(IFormFile file)
+        {
+            var imgSrc = "";
+
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+                    string s = Convert.ToBase64String(fileBytes);
+                    imgSrc = String.Format("data:image/jpg;base64,{0}", s);
+                }
+            }
+
+            return imgSrc;
+        }
+
+    }
+}
---
+++ CustomVisionAspDemo/Models/IndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomVisionAspDemo.Models
+{
+    public class IndexModel
+    {
+        public string Image { get; set; }
+
+        public string Prediction { get; set; }
+
+    }
+}
